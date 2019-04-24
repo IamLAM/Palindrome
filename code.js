f1cf2aa1e1eea2e3fb1d3317172346059e455707
IamLAM <ingeniero@luismonge.com.mx>
2019-04-24 12:22:22 -0700

    ordering and reversing string, with some functions such as: join and split

--- code.js
+++ code.js
@@ -1,12 +1,24 @@
 function palindrome(str) {
+    //remove all non alphanumeric characters
+    //lowercase or upper case
 
-    let s = str.toLowerCase();
+
+
+    /* let s = str.toLowerCase();
     rule = /[^a-z0-9]+/gi;
     let sr = s.replace(rule, "");
-    console.log(s);
+  */
+    let v = str.split("").reverse().join("");
+    if (v == str) {
+        return true;
+    } else {
+        return false;
+    }
+    console.log(str);
+    console.log(v);
 
 }
 
 
 
-console.log(palindrome("213 12321 Eye****"));
+palindrome("eye");
