a48f254e54e1634208982a3c04a3fdcb3cfa4311
IamLAM <ingeniero@luismonge.com.mx>
2019-04-24 13:46:57 -0700

    Delete alphanumerics, replace values with empty string and compare values

--- code.js
+++ code.js
@@ -1,24 +1,30 @@
 function palindrome(str) {
     //remove all non alphanumeric characters
-    //lowercase or upper case
 
-
-
-    /* let s = str.toLowerCase();
-    rule = /[^a-z0-9]+/gi;
-    let sr = s.replace(rule, "");
-  */
-    let v = str.split("").reverse().join("");
-    if (v == str) {
-        return true;
-    } else {
-        return false;
-    }
+    let codingRegex = /[^A-Za-z0-9]/g;
+    str = str.replace(codingRegex, "");
+    let i = 0;
+    let j = str.length - 1;
+    console.log(j);
     console.log(str);
-    console.log(v);
+    while (i < str.length) {
+        if (str[i].toLowerCase() == str[j].toLowerCase()) {
+            console.log("It is a coincidence");
+        } else {
+            console.log("It isn't a palindrome");
+            return false;
+        }
+        console.log("str1: " + str[i] + "str2: " + str[j]);
+        i++;
+        j--;
+    }
+    return true;
+    console.log(str);
+
 
 }
 
 
 
-palindrome("eye");
+palindrome("A man, a plan, a canal. Panama");
+console.log("Ok");
